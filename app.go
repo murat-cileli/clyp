c0ec3818445447e1a7b670aed958f8c7bd0f0a7d
Murat Çileli <murat.cileli@gmail.com>
2025-08-13 23:45:00 +0300

    WIP: Code refactoring.

---
+++ app.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	_ "embed"
+	"log"
+	"os"
+
+	"github.com/diamondburned/gotk4/pkg/gdk/v4"
+	"github.com/diamondburned/gotk4/pkg/gio/v2"
+	"github.com/diamondburned/gotk4/pkg/gtk/v4"
+	"github.com/diamondburned/gotk4/pkg/pango"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+//go:embed main.ui
+var uiXML string
+
+//go:embed style.css
+var cssData string
+
+type Application struct {
+}
+
+func (app *Application) init() {
+	gtkApp := gtk.NewApplication("com.github.diamondburned.gotk4-examples.gtk4.simple", gio.ApplicationFlagsNone)
+	gtkApp.ConnectActivate(func() { app.activate(gtkApp) })
+
+	if code := gtkApp.Run(os.Args); code > 0 {
+		os.Exit(code)
+	}
+}
+
+func (app *Application) activate(gtkApp *gtk.Application) {
+	app.loadCSS()
+
+	builder := gtk.NewBuilderFromString(uiXML)
+	window := builder.GetObject("GtkWindow").Cast().(*gtk.ApplicationWindow)
+
+	app.setupStyleSupport()
+
+	listBox := builder.GetObject("clipboard_list").Cast().(*gtk.ListBox)
+	app.setupListBox(listBox)
+	app.setupListBoxEvents(listBox)
+
+	window.SetApplication(gtkApp)
+	window.Show()
+}
+
+func (app *Application) loadCSS() {
+	cssProvider := gtk.NewCSSProvider()
+	cssProvider.LoadFromData(cssData)
+
+	display := gdk.DisplayGetDefault()
+	if display != nil {
+		gtk.StyleContextAddProviderForDisplay(display, cssProvider, gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+	}
+}
+
+func (app *Application) setupListBox(listBox *gtk.ListBox) {
+	items, err := getClipboardItems()
+	if err != nil {
+		log.Printf("Veritabanından veri alınırken hata: %v", err)
+		return
+	}
+
+	if len(items) == 0 {
+		// TODO: Boş veri mesajı ekle.
+	}
+
+	for i := 0; i < 30; i++ {
+		box := gtk.NewBox(gtk.OrientationVertical, 6)
+		box.SetMarginTop(12)
+		box.SetMarginBottom(12)
+		box.SetMarginStart(12)
+		box.SetMarginEnd(12)
+
+		contentLabel := gtk.NewLabel(items[i].Content)
+		contentLabel.SetWrap(true)
+		contentLabel.SetWrapMode(pango.WrapWord)
+		contentLabel.SetXAlign(0)
+		contentLabel.SetSelectable(true)
+		contentLabel.AddCSSClass("content-label")
+
+		dateLabel := gtk.NewLabel(items[i].DateTime)
+		dateLabel.SetXAlign(0)
+		dateLabel.AddCSSClass("dim-label")
+		dateLabel.AddCSSClass("caption")
+
+		box.Append(contentLabel)
+		box.Append(dateLabel)
+
+		row := gtk.NewListBoxRow()
+		row.SetChild(box)
+
+		listBox.Append(row)
+	}
+}
+
+func (app *Application) setupListBoxEvents(listBox *gtk.ListBox) {
+	listBox.ConnectRowActivated(func(row *gtk.ListBoxRow) {
+		copyRowContentToClipboard(row)
+	})
+
+	keyController := gtk.NewEventControllerKey()
+	keyController.ConnectKeyPressed(func(keyval, keycode uint, state gdk.ModifierType) bool {
+		if keyval == gdk.KEY_Return || keyval == gdk.KEY_KP_Enter {
+			selectedRow := listBox.SelectedRow()
+			if selectedRow != nil {
+				copyRowContentToClipboard(selectedRow)
+				return true // Event'i consume et
+			}
+		}
+		return false // Event'i başka handler'lara geçir
+	})
+
+	listBox.AddController(keyController)
+}
+
+func (app *Application) setupStyleSupport() {
+	gtkSettings := gtk.SettingsGetDefault()
+	gnomeSettings := gio.NewSettings("org.gnome.desktop.interface")
+	app.handleStyleChange(gtkSettings, gnomeSettings)
+	gnomeSettings.Connect("changed::color-scheme", func() {
+		app.handleStyleChange(gtkSettings, gnomeSettings)
+	})
+}
+
+func (app *Application) handleStyleChange(gtkSettings *gtk.Settings, gnomeSettings *gio.Settings) {
+	gtkSettings.SetObjectProperty("gtk-application-prefer-dark-theme", gnomeSettings.String("color-scheme") == "prefer-dark")
+}
--- main.go
+++ main.go
@@ -2,24 +2,13 @@ package main
 
 import (
 	"database/sql"
-	_ "embed"
-	"fmt"
 	"log"
-	"os"
-	"time"
 
 	"github.com/diamondburned/gotk4/pkg/gdk/v4"
-	"github.com/diamondburned/gotk4/pkg/gio/v2"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
-	"github.com/diamondburned/gotk4/pkg/pango"
-	_ "github.com/mattn/go-sqlite3"
 )
 
-//go:embed main.ui
-var uiXML string
-
-//go:embed style.css
-var cssData string
+var app Application
 
 // ClipboardItem veritabanından okunan clipboard verisini temsil eder
 type ClipboardItem struct {
@@ -32,114 +21,7 @@ type ClipboardItem struct {
 }
 
 func main() {
-	app := gtk.NewApplication("com.github.diamondburned.gotk4-examples.gtk4.simple", gio.ApplicationFlagsNone)
-	app.ConnectActivate(func() { activate(app) })
-
-	if code := app.Run(os.Args); code > 0 {
-		os.Exit(code)
-	}
-}
-
-func activate(app *gtk.Application) {
-	// CSS stillerini yükle
-	loadCSS()
-
-	builder := gtk.NewBuilderFromString(uiXML)
-	window := builder.GetObject("GtkWindow").Cast().(*gtk.ApplicationWindow)
-
-	// GNOME tema desteği için ayarları yapılandır
-	setupStyleSupport()
-
-	// ListBox'ı al ve örnek içerik ekle
-	listBox := builder.GetObject("clipboard_list").Cast().(*gtk.ListBox)
-	setupListBox(listBox)
-
-	// ListBox için event handler'ları ekle
-	setupListBoxEvents(listBox)
-
-	window.SetApplication(app)
-	window.Show()
-}
-
-// loadCSS CSS stillerini yükler
-func loadCSS() {
-	cssProvider := gtk.NewCSSProvider()
-	cssProvider.LoadFromData(cssData)
-
-	display := gdk.DisplayGetDefault()
-	if display != nil {
-		gtk.StyleContextAddProviderForDisplay(display, cssProvider, gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-	}
-}
-
-// setupListBox ListBox'ı veritabanından gelen verilerle doldurur
-func setupListBox(listBox *gtk.ListBox) {
-	items, err := getClipboardItems()
-	if err != nil {
-		log.Printf("Veritabanından veri alınırken hata: %v", err)
-		return
-	}
-
-	if len(items) == 0 {
-		// TODO: Boş veri mesajı ekle.
-	}
-
-	for _, item := range items {
-		// Her öğe için bir Box oluştur (dikey)
-		box := gtk.NewBox(gtk.OrientationVertical, 6)
-		box.SetMarginTop(12)
-		box.SetMarginBottom(12)
-		box.SetMarginStart(12)
-		box.SetMarginEnd(12)
-
-		// Ana içerik label'ı
-		contentLabel := gtk.NewLabel(item.Content)
-		contentLabel.SetWrap(true)
-		contentLabel.SetWrapMode(pango.WrapWord)
-		contentLabel.SetXAlign(0) // Sol hizalama
-		contentLabel.SetSelectable(true)
-		contentLabel.AddCSSClass("content-label")
-
-		// Tarih label'ı (alt başlık)
-		dateLabel := gtk.NewLabel(formatDateTime(item.DateTime))
-		dateLabel.SetXAlign(0) // Sol hizalama
-		dateLabel.AddCSSClass("dim-label")
-		dateLabel.AddCSSClass("caption")
-
-		// Box'a label'ları ekle
-		box.Append(contentLabel)
-		box.Append(dateLabel)
-
-		// ListBoxRow oluştur ve box'ı ekle
-		row := gtk.NewListBoxRow()
-		row.SetChild(box)
-
-		listBox.Append(row)
-	}
-}
-
-// setupListBoxEvents ListBox için event handler'ları ayarlar
-func setupListBoxEvents(listBox *gtk.ListBox) {
-	// Çift tıklama ve Enter tuşu için row-activated sinyalini dinle
-	listBox.ConnectRowActivated(func(row *gtk.ListBoxRow) {
-		copyRowContentToClipboard(row)
-	})
-
-	// Klavye event'leri için key controller ekle
-	keyController := gtk.NewEventControllerKey()
-	keyController.ConnectKeyPressed(func(keyval, keycode uint, state gdk.ModifierType) bool {
-		// Enter tuşuna basıldığında seçili satırı kopyala
-		if keyval == gdk.KEY_Return || keyval == gdk.KEY_KP_Enter {
-			selectedRow := listBox.SelectedRow()
-			if selectedRow != nil {
-				copyRowContentToClipboard(selectedRow)
-				return true // Event'i consume et
-			}
-		}
-		return false // Event'i başka handler'lara geçir
-	})
-
-	listBox.AddController(keyController)
+	app.init()
 }
 
 // copyRowContentToClipboard seçili satırın içeriğini panoya kopyalar
@@ -210,7 +92,6 @@ func getClipboardItems() ([]ClipboardItem, error) {
 		SELECT *
 		FROM clipboard
 		ORDER BY date_time DESC
-		LIMIT 30
 	`)
 	if err != nil {
 		return nil, err
@@ -221,58 +102,12 @@ func getClipboardItems() ([]ClipboardItem, error) {
 	var clipboardItem ClipboardItem
 	for rows.Next() {
 		if err := rows.Scan(&clipboardItem.ID, &clipboardItem.Type, &clipboardItem.DateTime, &clipboardItem.IsPinned, &clipboardItem.Content, &clipboardItem.IsEncrypted); err != nil {
-			log.Printf("Satır okunurken hata: %v", err)
 			continue
 		}
 		clipboardItems = append(clipboardItems, clipboardItem)
 	}
 
 	return clipboardItems, nil
-}
-
-// formatDateTime tarih string'ini kullanıcı dostu formata çevirir
-func formatDateTime(dateTimeStr string) string {
-	// SQLite'dan gelen tarih formatını parse et
-	layouts := []string{
-		"2006-01-02 15:04:05",
-		"2006-01-02T15:04:05Z",
-		"2006-01-02T15:04:05",
-		"2006-01-02 15:04:05.000",
-	}
-
-	var parsedTime time.Time
-	var err error
-
-	for _, layout := range layouts {
-		parsedTime, err = time.Parse(layout, dateTimeStr)
-		if err == nil {
-			break
-		}
-	}
-
-	if err != nil {
-		// Parse edilemezse orijinal string'i döndür
-		return dateTimeStr
-	}
-
-	// Türkçe tarih formatında döndür
-	now := time.Now()
-	diff := now.Sub(parsedTime)
-
-	if diff < time.Minute {
-		return "Az önce"
-	} else if diff < time.Hour {
-		minutes := int(diff.Minutes())
-		return fmt.Sprintf("%d dakika önce", minutes)
-	} else if diff < 24*time.Hour {
-		hours := int(diff.Hours())
-		return fmt.Sprintf("%d saat önce", hours)
-	} else if diff < 7*24*time.Hour {
-		days := int(diff.Hours() / 24)
-		return fmt.Sprintf("%d gün önce", days)
-	} else {
-		return parsedTime.Format("02.01.2006 15:04")
-	}
 }
 
 // createClipboardTable clipboard tablosunu oluşturur
@@ -286,21 +121,4 @@ func createClipboardTable(db *sql.DB) error {
 
 	_, err := db.Exec(query)
 	return err
-}
-
-func setupStyleSupport() {
-	gtkSettings := gtk.SettingsGetDefault()
-	gnomeSettings := gio.NewSettings("org.gnome.desktop.interface")
-
-	gnomeSettings.Connect("changed::color-scheme", func() {
-		handleStyleChange(gtkSettings, gnomeSettings)
-	})
-
-	handleStyleChange(gtkSettings, gnomeSettings)
-}
-
-func handleStyleChange(gtkSettings *gtk.Settings, gnomeSettings *gio.Settings) {
-	colorScheme := gnomeSettings.String("color-scheme")
-	preferDark := colorScheme == "prefer-dark"
-	gtkSettings.SetObjectProperty("gtk-application-prefer-dark-theme", preferDark)
 }
